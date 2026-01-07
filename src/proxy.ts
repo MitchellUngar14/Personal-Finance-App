907a65b47e05553c0f7cf0fefa35354e237c5fbe
Mitchell Ungar <mitchell.ungar@gmail.com>
2026-01-07 15:28:17 -0500

    Fixing proxy issue with app download

--- src/proxy.ts
+++ src/proxy.ts
@@ -39,7 +39,8 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public files (public folder)
+     * - PWA files (manifest.json, sw.js, workbox)
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|workbox-.*\\.js|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)",
   ],
 };
